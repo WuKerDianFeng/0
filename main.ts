ffb7dd5a487b37e2f1084da3d3740912c293270b
WuKerDianFeng <101851142+WuKerDianFeng@users.noreply.github.com>
2022-03-18 11:39:18 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+let 溫度 = 0
+let 濕度 = 0
 basic.forever(function () {
-    basic.showNumber(0)
+    溫度 = pins.analogReadPin(AnalogPin.P0)
+    濕度 = pins.analogReadPin(AnalogPin.P10)
+    basic.showString("HUMI")
+    basic.showNumber(濕度)
+    basic.clearScreen()
+    if (濕度 < 500) {
+        pins.servoWritePin(AnalogPin.P16, 90)
+        basic.pause(5000)
+        pins.servoWritePin(AnalogPin.P16, 0)
+        basic.pause(5000)
+    }
+    basic.clearScreen()
+    basic.showString("TEMP")
+    basic.showNumber(溫度)
+    // 預計會開燈，增加溫度
+    if (溫度 < 27) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+    basic.clearScreen()
 })
