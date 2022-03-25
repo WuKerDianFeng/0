ee6925a6dbda43a13a004521d2c1b49b0bb922bc
WuKerDianFeng <101851142+WuKerDianFeng@users.noreply.github.com>
2022-03-25 10:37:29 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,8 +17,10 @@ basic.forever(function () {
     basic.showNumber(溫度)
     // 預計會開燈，增加溫度
     if (溫度 < 27) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
         basic.showIcon(IconNames.No)
     } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
         basic.showIcon(IconNames.Yes)
     }
     basic.clearScreen()
